6b3849fc33abb18e806bbe49dc37eabc60f18935
Novian Rizky Imansyah <novianri36@gmail.com>
2024-02-13 10:55:43 +0700

    menambahkan list

--- index .php
+++ index .php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti vero quae cumque soluta, atque iusto distinctio culpa a. Debitis, aspernatur distinctio dicta fugiat dolor reprehenderit! Quod, nobis tenetur? Soluta, distinctio.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
